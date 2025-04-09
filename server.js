a3fd901b2488754e2ad71af2f9908d4c5d23c422
adivs1102 <101512203+adivs1102@users.noreply.github.com>
2025-04-09 14:00:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,10 +26,14 @@ app.use("/api/v1/users", require("./routes/userRoute"));
 app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 
 //static files
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
 
 //port
